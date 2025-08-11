2f6af4f98ca39a6773be4a17bf99e7908d6e7e97
maximilien <niyibizimaximilien@gmail.com>
2025-08-11 14:59:00 +0200

    .

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -13,7 +12,7 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
     finally:
         if 'cursor' in locals():
@@ -21,5 +20,4 @@ def create_database():
         if 'connection' in locals() and connection.is_connected():
             connection.close()
 
-if __name__ == "__main__":
-    create_database()
+if __name__ ==
