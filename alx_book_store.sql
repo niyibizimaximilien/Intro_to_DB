1169c2cf8a06b45fb92096421a8e4246c49307f0
maximilien <niyibizimaximilien@gmail.com>
2025-08-11 14:55:29 +0200

    .

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,4 +35,4 @@ CREATE TABLE Order_Details (
     book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
